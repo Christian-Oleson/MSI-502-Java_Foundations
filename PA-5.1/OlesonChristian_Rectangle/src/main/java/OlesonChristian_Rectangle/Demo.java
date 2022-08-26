494dc8b053668ca5fd70918aa32b417d016bc194
Christian Oleson <Christian.Oleson@ImagineLearning.com>
2022-08-25 21:39:54 -0500

    PA 5.1 - Completed Assignment (#6)
    * PA 5.1 - Completed Assignment
    
    * 5.1 - Minor fixes
    
    * 5.1

---
+++ PA-5.1/OlesonChristian_Rectangle/src/main/java/OlesonChristian_Rectangle/Demo.java
@@ -0,0 +1,62 @@
+package OlesonChristian_Rectangle;
+
+import java.util.Arrays;
+
+/** Demo class
+* @author Christian Oleson
+* @version 1.0
+* Demos the Rectangle and RectPrism classes, showing off all methods, getters, and setters.
+ */
+public class Demo {
+    public static void main(String[] args) {
+        var arrayOfRectangles = new Rectangle[]
+            {
+                new Rectangle(1, 1),
+                new Rectangle(2, 2),
+                new Rectangle(3, 3),
+                new Rectangle(4, 4),
+                new Rectangle(5, 5),
+                new RectPrism(1, 2, 3),
+                new RectPrism(2, 4, 6),
+                new RectPrism(3, 6, 9),
+                new RectPrism(4, 8, 12),
+                new RectPrism(5, 10, 15)
+            };
+        var countRectangularPrisms = Arrays.stream(arrayOfRectangles).filter(rectangle -> rectangle instanceof RectPrism).count();
+        // Show the count of each type in the array to compare with the count of each type in the Rectangle and RectPrism classes.
+        System.out.println("There are " + arrayOfRectangles.length + " rectangles in the array.");
+        System.out.println("There are " + countRectangularPrisms + " rectangular prisms in the array.");
+        // Show that the count variables in the Rectangle and RectPrism classes have the correct values.
+        System.out.println("The count of rectangles created is " + Rectangle.getCountRectanglesCreated());
+        System.out.println("The count of rectPrisms created is " + RectPrism.getCountRectPrismsCreated());
+        System.out.println("-------------------------------------------------------------------------------------");
+
+        // Iterate through the objects to show everything works as expected.
+        for (var rectangle : arrayOfRectangles) {
+            System.out.println("Object: " + rectangle.hashCode());
+            var height = rectangle.getHeight();
+            var width = rectangle.getWidth();
+            printValuesOfRectangle(rectangle, "Original");
+            rectangle.setHeight(height * height);
+            rectangle.setWidth(width * width);
+            printValuesOfRectangle(rectangle, "Updated");
+            System.out.println("-------------------------------------------------------------------------------------");
+        }
+    }
+
+    /** Prints the values of the rectangle.
+     * @param rectangle The rectangle to print the values of.
+     * @param type The message to print before the values of the rectangle.
+     */
+    private static void printValuesOfRectangle(Rectangle rectangle, String type) {
+        System.out.println(type + " Surface Area: " + rectangle.computeSurfaceArea());
+        System.out.println(type + " Height: " + rectangle.getHeight());
+        System.out.println(type + " Width: " + rectangle.getWidth());
+        if (rectangle instanceof RectPrism) {
+            var depth = ((RectPrism) rectangle).getDepth();
+            System.out.println(type + " Depth: " + depth);
+            System.out.println(type + " Volume: " + ((RectPrism) rectangle).computeVolume());
+            ((RectPrism) rectangle).setDepth(depth * depth);
+        }
+    }
+}
---
+++ PA-5.1/OlesonChristian_Rectangle/src/main/java/OlesonChristian_Rectangle/Rectangle.java
@@ -0,0 +1,67 @@
+package OlesonChristian_Rectangle;
+
+/**
+@author Christian Oleson
+@version 1.0
+The Rectangle class which creates a rectangle
+ */
+public class Rectangle {
+    private int height;
+    private int width;
+    private static int countRectanglesCreated = 0;
+
+    public Rectangle(int height, int width) {
+        setHeight(height);
+        setWidth(width);
+        countRectanglesCreated++;
+    }
+
+    /**
+    @return the height of the Rectangle
+     */
+    public int getHeight() {
+        return height;
+    }
+
+    /**
+    @param height set the height of the Rectangle
+     */
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    /**
+    @return the width of the Rectangle
+     */
+    public int getWidth() {
+        return width;
+    }
+
+    /**
+    @param width set the width of the Rectangle
+     */
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    /**
+    @return the count of rectangles created
+     */
+    public static int getCountRectanglesCreated() {
+        return countRectanglesCreated;
+    }
+
+    /**
+    @return the surface area of the rectangle
+     */
+    public int computeSurfaceArea() {
+        return getHeight() * getWidth();
+    }
+
+    /**
+    @return the string representation of the rectangle
+     */
+    public String toString() {
+        return "Rectangle [\n\theight = " + height + ", \n\twidth = " + width + "\n]";
+    }
+}
